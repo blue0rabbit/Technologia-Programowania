d00017f01e7c31e583df7de97f3ab1b540ee5945
blue-rabbit <blue-rabbit@bluerabbit>
2016-10-21 18:08:42 +0200

    Make make

---
+++ src/Cards/DeckTestJunit4.java
@@ -0,0 +1,40 @@
+package Cards;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.*;
+
+public class DeckTestJunit4 
+{
+	Deck deck;
+	
+	@Before
+	
+	public void runOnceBeforeEverything()
+	{
+		deck = new Deck("Y");
+		deck = new Deck("N");
+	}
+	
+	@Test(timeout=1000)
+	public void TimeoutTest() throws Exception
+	{
+		Deck deck2 = new Deck("N");
+		deck.DeckGenerate();
+	}
+	
+	@Test(expected = Exception.Desk)
+	public void ExceptionChecking()
+	{
+		System.out.println(deck.DeckRandomize().get(122).showSign());
+	}
+	
+	@Ignore
+	
+	public void IgnoreTest()
+	{
+		System.out.println("Staph igoring me!");
+	}
+	
+	
+}
